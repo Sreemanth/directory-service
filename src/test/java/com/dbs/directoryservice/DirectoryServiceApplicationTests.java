f3cb8e0a046a2939911ea1d82e27df18967767b4
Sreemanth Pulagam <sreemanth.pulagam@yahoo.com>
2020-06-10 23:21:41 +0530

    ContextLoads- Service and controller

--- src/test/java/com/dbs/directoryservice/DirectoryServiceApplicationTests.java
+++ src/test/java/com/dbs/directoryservice/DirectoryServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.dbs.directoryservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dbs.directoryservice.config.FileServiceProperties;
+import com.dbs.directoryservice.controller.FileController;
 
 @SpringBootTest
 class DirectoryServiceApplicationTests {
 
+	@Autowired
+	private FileController fileController;
+	
+	@Autowired
+	private FileServiceProperties fileServiceProperties;
+
 	@Test
 	void contextLoads() {
+		
+		assertThat(fileController).isNotNull();
+		assertThat(fileServiceProperties).isNotNull();
 	}
 
 }
